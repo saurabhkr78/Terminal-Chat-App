050731244281bb1b10484654a70a6b74c158cc7b
Saurabh Kumar <112249296+saurabhkr78@users.noreply.github.com>
2024-06-12 14:09:36 +0530

    Update server.py
    minor tweaks in server side

--- server.py
+++ server.py
@@ -1,64 +1,89 @@
 import socket
-import threading
+import select
+import sys
+
+# Constants
+BACKLOG = 10
+PORT = 3490
+
+# Utility function to get IP address
+def get_ip_address(sockaddr):
+    return sockaddr[0]
 
 # Server setup
-host = '127.0.0.1'  # Localhost
-port = 12345        # Arbitrary non-privileged port
-
-# Create a socket object
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server_socket.bind(('0.0.0.0', PORT))
+server_socket.listen(BACKLOG)
 
-# Bind the socket to the address and port
-server_socket.bind((host, port))
+print("Server is running...")
 
-# Listen for incoming connections (5 is the max number of queued connections)
-server_socket.listen(5)
+# Variables
+inputs = [server_socket]
+clients = {}
+nicked_users = {}
+super_users = set()
+password = "sherbetlemon"
 
-clients = []
-nicknames = []
+# Helper functions
+def broadcast_message(message, exclude_socket=None):
+    for sock in inputs:
+        if sock != server_socket and sock != exclude_socket:
+            try:
+                sock.send(message.encode())
+            except Exception as e:
+                print(f"Error sending message: {e}")
+                sock.close()
+                inputs.remove(sock)
 
-# Broadcast function to send messages to all clients
-def broadcast(message):
-    for client in clients:
-        client.send(message)
+def handle_new_connection(server_socket):
+    client_socket, client_address = server_socket.accept()
+    print(f"Established connection to {get_ip_address(client_address)}")
+    client_socket.send("Welcome to the chat!\nEnter username: ".encode())
+    inputs.append(client_socket)
 
-# Function to handle each client
-def handle_client(client):
-    while True:
-        try:
-            # Receive message from client
-            message = client.recv(1024)
-            broadcast(message)
-        except:
-            # Remove and close the client connection
-            index = clients.index(client)
-            clients.remove(client)
-            client.close()
-            nickname = nicknames[index]
-            broadcast(f'{nickname} has left the chat.'.encode('utf-8'))
-            nicknames.remove(nickname)
-            break
+def handle_client_message(sock):
+    try:
+        message = sock.recv(1024).decode().strip()
+        if not message:
+            raise ConnectionResetError
+        
+        if sock in nicked_users:
+            username = nicked_users[sock]
+            if username in super_users:
+                if message.startswith("ban "):
+                    user_to_ban = message.split(" ", 1)[1]
+                    for client, nick in nicked_users.items():
+                        if nick == user_to_ban:
+                            client.send("You have been banned!\n".encode())
+                            client.close()
+                            inputs.remove(client)
+                            del nicked_users[client]
+                            broadcast_message(f"{user_to_ban} has been banned!\n")
+                            return
+            broadcast_message(f"{username}: {message}\n", sock)
+        else:
+            if message == password:
+                sock.send("Welcome admin! Mention your codename: ".encode())
+                super_users.add(sock)
+            else:
+                nicked_users[sock] = message
+                broadcast_message(f"{message} has joined the chat!\n")
 
-# Main function to receive clients
-def receive_clients():
-    while True:
-        # Accept a new connection
-        client, address = server_socket.accept()
-        print(f'Connected with {str(address)}')
+    except Exception as e:
+        print(f"Error: {e}")
+        if sock in nicked_users:
+            username = nicked_users[sock]
+            broadcast_message(f"{username} has left the chat.\n")
+            del nicked_users[sock]
+        sock.close()
+        inputs.remove(sock)
 
-        # Request and store nickname
-        client.send('NICK'.encode('utf-8'))
-        nickname = client.recv(1024).decode('utf-8')
-        nicknames.append(nickname)
-        clients.append(client)
-
-        print(f'Nickname of the client is {nickname}')
-        broadcast(f'{nickname} has joined the chat!'.encode('utf-8'))
-        client.send('Connected to the server!'.encode('utf-8'))
-
-        # Start a new thread to handle the client
-        thread = threading.Thread(target=handle_client, args=(client,))
-        thread.start()
-
-print('Server is listening...')
-receive_clients()
+# Main server loop
+while True:
+    read_sockets, _, _ = select.select(inputs, [], [])
+    for sock in read_sockets:
+        if sock == server_socket:
+            handle_new_connection(server_socket)
+        else:
+            handle_client_message(sock)
